5aefbb6cb535a5dcaa0194048948ea15c5354f55
Dede Gunawan <dede@unsil.ac.id>
2019-12-05 21:00:42 +0700

    Initial Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('adminlte::page')
+
+@section('content_header')
+    <h1 class="m-0 text-dark">Selamat Datang</h1>
+@stop
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <p class="mb-0">
+                        Selamat datang di aplikasi SPK menggunakan metode WP
+                    </p>
+                    <p class="mb-0">
+                        Silahkan pilih menu disamping untuk melakukan pengolahan data
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
